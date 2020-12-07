ff0a2cc6ef136b714ae08efaf61eefa70020b785
Leonardo Santos dos Santos <leonardo.felipe.santos@usp.br>
2020-12-06 21:07:32 -0300

    Update State Space for control design

--- MATLAB/kalmanStateSpaceModellingHumInput.m
+++ MATLAB/kalmanStateSpaceModellingHumInput.m
@@ -34,6 +34,17 @@ C = [eye(sensor_dim)]; C(4,4) = N; C(5,5) = N; C(1,1) = 0;
 D = zeros(sensor_dim, control_dim); D(1,1) = 1;
 
 poles = eig(A)  % check open-loop stability
+
+% Check Controllability & Observability:
+if (rank(ctrb(A,B)) == state_dim) && (rank(obsv(A,C)) == state_dim)
+    disp('System is controllable and observable.')
+elseif (rank(ctrb(A,B)) == state_dim) && (rank(obsv(A,C)) ~= state_dim)
+    disp('System is controllable and not observable.')
+elseif (rank(ctrb(A,B)) ~= state_dim) && (rank(obsv(A,C)) == state_dim)
+    disp('System is not controllable and is observable.')
+else
+    disp('System is neither controllable and observable.')
+end
 %% State Space definition, 
 % considering the different sensors 
 % sample rate as output delay:
@@ -45,3 +56,19 @@ Ts = 1; % [ms]
 % disOpt = c2dOptions('Method','tustin','PrewarpFrequency',2*pi*sea_bw_freq);
 % discreteSys = c2d(Sys,Ts,disOpt)
 discreteSys = c2d(Sys,Ts)
+
+%% Control Design
+epos_Ki = 1.190; epos_Kp = 11.900;
+
+damping_d = 0.900;
+stiffness_d = 3.874;
+stiffness_lower = 3.8422;
+
+eposVelocityController = pid(epos_Kp,epos_Ki,0,0,0.001);
+
+s = tf('s')
+
+CAC = (1 - stiffness_d/Ks)*s / (stiffness_d + s*damping_d);
+CAC = c2d(CAC,0.001);
+
+stiffness_lower = damping_d*(epos_Ki/epos_Kp - damping_d/(Ja*(1 - stiffness_d/Ks)) - epos_Kp/Ja);
