d7010bb7ba85ec4f2a0e2def73f48cadc2107647
Leonardo Felipe Dos Santos <leonardo.flss@hotmail.com>
2021-03-20 18:30:10 -0300

    Starting KF with Int Force estimation

--- MATLAB/kalmanSSHumInput.m
+++ MATLAB/kalmanSSHumInput.m
@@ -18,9 +18,9 @@ Beq = 60;            % N.m.s/rad (maybe is 30, I.O.)
 Ja  = 0.47;          % Kg.m^2
 
 % State vector is  [x_h x_e x_a \dot{x_e} \dot{x_a}]
-% Sensor vector is [\dot{x_h} x_e x_m \dot{x_e} \dot{x_m}]
+% Sensor vector is [tau_i \dot{x_h} x_e x_m \dot{x_e} \dot{x_m}]
 state_dim  = 5;
-sensor_dim = 5;
+sensor_dim = 6;
 control_dim = 3;
 
 % State Space matrices
@@ -33,8 +33,9 @@ A = [zeros(1,state_dim); ...
 
 B = [1 0 0; zeros(2,control_dim); 0 -1/Je 0; 0 0 N*KI/Ja];
 
-C = [eye(sensor_dim)]; C(1,1) = 0; C(3,3) = N; C(5,5) = N;
-D = zeros(sensor_dim, control_dim); D(1,1) = 1;
+C = [zeros(sensor_dim,state_dim)]; 
+C(1,1) = -Ka; C(1,2) = Ka; C(3,2) = 1; C(4,3) = N; C(5,4) = 1; C(6,5) = N;
+D = zeros(sensor_dim, control_dim); D(2,1) = 1;
 
 poles = eig(A)  % check open-loop stability
 
@@ -52,7 +53,7 @@ end
 % considering the different sensors 
 % sample rate as output delay:
 imu_delay = 8; % [ms]
-Sys = ss(A,B,C,D,'TimeUnit','milliseconds','OutputDelay',[imu_delay 0 imu_delay 0 0]');
+Sys = ss(A,B,C,D,'TimeUnit','milliseconds','OutputDelay',[0 imu_delay 0 imu_delay 0 0]');
 % Discretization
 Ts = 1; % [ms]
 % sea_bw_freq = 9.6/1000; % [kHz]
